52213000b77d5f0c5db2de87d8c13dd32944663b
Peter Seremetis <pswriter5348@gmail.com>
2018-01-18 01:32:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
+ var animal = 'dog'
 function myAnimal() {
-  var animal = 'dog';
-  return function yourAnimal() { 
-    var animal2 = 'cat';
-    return animal
-  }
+  return animal
 }
 
+function yourAnimal() {
+  var animal = 'cat'
+  return animal
+}
+
+1.	function outerFunction() {
+2.	  var innerVariable = "I'm sort of a secret.";
+3.	 
+4.	  return function innerScope() {
+5.	    var inaccessible = "Nothing can touch me.";
+6.	 
+7.	    return innerVariable;
+8.	  }
+9.	}
 
 
  // How can we make sure that this function
